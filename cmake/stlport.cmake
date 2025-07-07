2ee43c34eeab4718740e49028ad7b8675abaf975
aliendroid1 <9aliquraishi9@gmail.com>
2025-07-07 02:08:44 -0500

    [CMAKE] Upgrade to stlport 4.6.2

--- cmake/stlport.cmake
+++ cmake/stlport.cmake
@@ -1,8 +1,9 @@
 FetchContent_Declare(
     stlport
-    GIT_REPOSITORY https://github.com/TheSuperHackers/stlport-4.5.3.git
-    GIT_TAG        501824065f5b1eab77ca7f53ed3a227acf965eb1
+    URL https://downloads.sourceforge.net/project/stlport/STLport%20archive/STLport%204/STLport-4.6.2.tar.gz
 )
-
 FetchContent_MakeAvailable(stlport)
-target_compile_definitions(stlport INTERFACE USING_STLPORT=1)
+
+add_library(stlport INTERFACE)
+target_include_directories(stlport INTERFACE "${stlport_SOURCE_DIR}/stlport")
+target_compile_definitions(stlport INTERFACE USING_STLPORT=1 _STLP_NO_NEW_IOSTREAMS=1)
